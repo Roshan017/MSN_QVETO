8c999f4f4be1fcf1eb87f2890dd463f04db46d48
Roshan017 <roshanprakashmathew@gmail.com>
2025-07-09 13:15:43 +0530

    Sujay Trending news Component

--- msn-frontend/src/App.jsx
+++ msn-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import NewsList from './components/NewsList';
+import TrendingNews from './sections/TrendingNews';
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <div className=" flex flex-col min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">MSN NEWS APP</h1>
       <NewsList/>
+      <TrendingNews/>
     </div>
   );
 }
