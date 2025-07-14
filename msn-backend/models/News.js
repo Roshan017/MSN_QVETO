2cb3a2a5a0a80d3c4370a4f0bc2df8e7df1f3c85
Anirudha81 <ojhaanirudh81@gmail.com>
2025-07-14 14:26:38 +0530

    Update News.js

--- msn-backend/models/News.js
+++ msn-backend/models/News.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mo
+
+
+const mongoose = require("mongoose");
 
 const newsSchema = new mongoose.Schema({
-    headline: {type: String, required: true},
-    desc: String,
-    image: String,
-    category: String,
+  title: { type: String, required: true },
+  category: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String, required: true },
+  date: { type: Date, required: true },
+}, { timestamps: true });
 
-    createdAt: {type:Date, default: Date.now}
-})
-
-
-module.exports = mongoose.model('News',newsSchema)
+module.exports = mongoose.model("News", newsSchema);
